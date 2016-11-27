1e90bfe4e4b1a186e20b89245a22cda194d7ece0
Dmitri Peredera <dmitri.peredera@gmail.com>
2016-11-27 21:23:42 +0100

    Created project with 1.0.1.

---
+++ src/EfCoreDemo_SQLite/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EfCoreDemo_SQLite
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
