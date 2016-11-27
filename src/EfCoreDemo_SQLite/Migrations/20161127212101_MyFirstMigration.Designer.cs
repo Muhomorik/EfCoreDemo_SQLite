811ca449a49d933df4522681d24ea5d691a07107
Dmitri Peredera <dmitri.peredera@gmail.com>
2016-11-27 22:23:55 +0100

    + First db migration.

---
+++ src/EfCoreDemo_SQLite/Migrations/20161127212101_MyFirstMigration.Designer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using EfCoreDemo_SQLite.TutorialModels;
+
+namespace EfCoreDemo_SQLite.Migrations
+{
+    [DbContext(typeof(BloggingContext))]
+    [Migration("20161127212101_MyFirstMigration")]
+    partial class MyFirstMigration
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.0-rtm-22752");
+
+            modelBuilder.Entity("EfCoreDemo_SQLite.TutorialModels.Blog", b =>
+                {
+                    b.Property<int>("BlogId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Url");
+
+                    b.HasKey("BlogId");
+
+                    b.ToTable("Blogs");
+                });
+
+            modelBuilder.Entity("EfCoreDemo_SQLite.TutorialModels.Post", b =>
+                {
+                    b.Property<int>("PostId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("BlogId");
+
+                    b.Property<string>("Content");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("PostId");
+
+                    b.HasIndex("BlogId");
+
+                    b.ToTable("Posts");
+                });
+
+            modelBuilder.Entity("EfCoreDemo_SQLite.TutorialModels.Post", b =>
+                {
+                    b.HasOne("EfCoreDemo_SQLite.TutorialModels.Blog", "Blog")
+                        .WithMany("Posts")
+                        .HasForeignKey("BlogId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
